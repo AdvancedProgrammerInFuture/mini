c2d5485128953baabf0cabf7cf18eec85a290786
AdvancedProgrammerInFuture <43684350+AdvancedProgrammerInFuture@users.noreply.github.com>
2019-04-26 04:33:42 +0300

    Add files via upload

--- linked_list.c
+++ linked_list.c
@@ -11,10 +11,11 @@ typedef struct carType {
 } Car; 
 
 Car *Scanlist(Car *headpointer, int search_ID);
-
+void AddEntry(Car *headpointer);
+void DeleteEntry(Car *headpointer)	
 int main() {
 	int operation = 0 // current operation to be performed
-	Car carBase; // is an empty head node
+	Car carBase; // is an 		empty head node
 	
 	carBase.next = NULL;
 	
@@ -56,5 +57,48 @@ Car *Scanlist(Car *headPointer, int search_ID)
 `	
 	return previous;
 }
+
+void AddEntry(Car *headPointer)
+{
+	Car *newNode; // Points to new Car-info
+	Car *nextNode; // Points to Car to follow new one
+	Car *prevNode; // Points to Car before this one
+
+	newNode = (Car *) malloc(sizeof(Car));
+	
+	if (newNode == NULL) {
+		printf("Error: could not allocate a new node\n");
+		exit(1);
+	}
+	
+	printf("Enter the following info about the car.\n");
+	printf("Separate each field by white space:\n");
+	printf("vehicle_id make model year mileage cost\n");
+	
+	scanf("%d %s %s %d %d %lf", &newNode->id_of_vehicle, &newNode->manufacturer, &newNode->model_of_veh, &newNode->year_of_manufacturer, &newNode->meleage, &newNode->cost); 	
+
+	prevNode = ScanList(headPointer, newNode->id_of_vehicle);
+	nextNode = prevNode->next;
+	
+	if ((nextNode == NULL) || (nextNode->id_of_vehicle != newNode->id_of_vehicle)) {
+		prevNode->next = newNode;
+		newNode->next = nextNode;
+		printf("Entry added.\n\n");
+	}
+	else {
+		printf("That car already exists in the database!\n");
+		printf("Entry not added.\n\n");
+		free(newNode);
+	}
+}
+
+void DeleteEntry(Car *headPointer) 
+{
+	int id_of_vehicle;
+	Car *delNode; // Points to node that need to delete
+	Car *prevNode; // Points to node that prior to delNode
+
+	printf("Enter id of the vehicle of the car that need to delete");
+	
 
 
